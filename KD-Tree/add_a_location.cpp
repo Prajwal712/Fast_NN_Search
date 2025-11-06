34e2db29c47a52604b0e30ecf0ae54e5335d99b9
Prajwal712 <prajwalpathade7@gmail.com>
2025-11-07 00:18:38 +0530

    added insert func for kd_tree

---
+++ KD-Tree/add_a_location.cpp
@@ -0,0 +1,17 @@
+#include "kd_tree.h"
+
+KDNode* insert(KDNode * root , vector<double> point , int depth = 0){
+    if(root == NULL){
+        KDNode * newNode = new KDNode(point);
+        return newNode;
+    }
+    int axis = depth % 2;
+    if(point[axis] < root->point[axis]){
+        root->left = insert(root->left , point , depth+1);
+    }
+    else{
+        root->right = insert(root->right , point , depth + 1);
+    }
+    
+    return root;
+}
